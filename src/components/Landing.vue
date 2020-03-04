fa7b6d8749ae8ad97e6c1f31a5f3a3b0a460504e
Erlend Ellefsen <ekbe_97@hotmail.no>
2020-03-04 21:50:40 +0100

    tests

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -4,6 +4,6 @@
       <Heading :title="$t('landing.TITLE')" />
       <Description :description="$t('landing.DESCRIPTION')" />
     </v-layout>
-    <ProjectDescription />
+    <ProjectDescription :test="$t('test.TEST')" />
   </v-container>
 </template>
