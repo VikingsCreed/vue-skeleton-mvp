7edcdbd438d73d7098e17be2e84a1021c3773e0a
Erlend Ellefsen <ekbe_97@hotmail.no>
2020-03-04 12:30:43 +0100

    adding files

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+// server.js
 const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('An alligator approaches!')
-})
-
-app.listen(3000, () => console.log('Gator app listening on port 3000!'))
+const path = require('path')
+const serveStatic = require('serve-static')
+app = express()
+app.use(serveStatic(`${__dirname}/dist`))
+const port = process.env.PORT || 5000
+app.listen(port)
+console.log(`server started ${port}`)
