325353a1d14a039d6719c4819235d83adeed0f4c
Robin Ellingsen <robin-elli@hotmail.com>
2020-03-06 12:02:23 +0100

    Jobbet med flerspråklighet, tyks

--- src/plugins/veevalidate.js
+++ src/plugins/veevalidate.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 import en from 'vee-validate/dist/locale/en'
 import es from 'vee-validate/dist/locale/es'
+import de from 'vee-validate/dist/locale/de'
 
 const veeValidateConfig = {
   locale: JSON.parse(localStorage.getItem('locale')) || 'en',
   dictionary: {
     en,
-    es
+    es,
+    de
   }
 }
 
